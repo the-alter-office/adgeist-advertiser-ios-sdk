147fccbb3b6fa3ba1feda53d155d50f7a5361eab
joynal-alter <joynal@thealteroffice.com>
2026-09-16 14:44:03 +0530

    Update package.swift

--- package.swift
+++ package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdgeistAdvertiserSDK",
-            url: "https://github.com/the-alter-office/adgeist-advertiser-ios-sdk/releases/download/0.0.5/AdgeistAdvertiserSDK-0.0.5.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            url: "https://github.com/the-alter-office/adgeist-advertiser-ios-sdk/releases/download/0.0.8/AdgeistAdvertiserSDK.xcframework.zip",
+            checksum: "cc44e663d252d95e01ffad5714d0c65dd86b7624b160afac1aa48eb65a2376c2"
         )
     ]
 )
